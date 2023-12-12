11db348a2b8edad6daae9fec712b014c45741641
ryleu <69326171+ryleu@users.noreply.github.com>
2023-12-12 07:52:41 -0500

    finish fixing glow lights recipes 2/2

--- packwiz/global_packs/required_data/christmas/data/snowyspirit/recipes/gen_glow_lights.py
+++ packwiz/global_packs/required_data/christmas/data/snowyspirit/recipes/gen_glow_lights.py
@@ -23,26 +23,26 @@ template = \
     }}
 }}"""
 
-colors = [
-    ("red_dye", "red"),
-    ("orange_dye", "orange"),
-    ("yellow_dye", "yellow"),
-    ("green_dye", "green"),
-    ("blue_dye", "blue"),
-    ("purple_dye", "purple"),
-    ("magenta_dye", "magenta"),
-    ("light_blue_dye", "light_blue"),
-    ("cyan_dye", "cyan"),
-    ("lime_dye", "lime"),
-    ("brown_dye", "brown"),
-    ("black_dye", "black"),
-    ("white_dye", "white"),
-    ("pink_dye", "pink"),
-    ("gray_dye", "gray"),
-    ("light_gray_dye", "light_gray"),
-    ("diamond", "prismatic")
-]
+types = {
+    "red_dye": "red",
+    "orange_dye": "orange",
+    "yellow_dye": "yellow",
+    "green_dye": "green",
+    "blue_dye": "blue",
+    "purple_dye": "purple",
+    "magenta_dye": "magenta",
+    "light_blue_dye": "light_blue",
+    "cyan_dye": "cyan",
+    "lime_dye": "lime",
+    "brown_dye": "brown",
+    "black_dye": "black",
+    "white_dye": "white",
+    "pink_dye": "pink",
+    "gray_dye": "gray",
+    "light_gray_dye": "light_gray",
+    "diamond": "prismatic"
+}
 
-for i in colors:
-    with open(f"glow_lights_{i[1]}.json", "w") as file:
-        file.write(template.format(ingredient=i[0], variety=i[1]))
+for ingredient, variety in types.items():
+    with open(f"glow_lights_{variety}.json", "w") as file:
+        file.write(template.format(ingredient=ingredient, variety=variety))
