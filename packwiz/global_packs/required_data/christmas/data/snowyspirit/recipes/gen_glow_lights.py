1f3dfb97ba7429f570c24089fa5fce6562745598
ryleu <69326171+ryleu@users.noreply.github.com>
2023-12-12 07:48:03 -0500

    finish fixing glow lights recipes

--- packwiz/global_packs/required_data/christmas/data/snowyspirit/recipes/gen_glow_lights.py
+++ packwiz/global_packs/required_data/christmas/data/snowyspirit/recipes/gen_glow_lights.py
@@ -1,47 +1,48 @@
 #!/bin/env python3
 
 template = \
-"""{
+"""{{
     "type": "minecraft:crafting_shaped",
     "pattern": [
         "G#S"
     ],
-    "key": {
-        "G": {
+    "key": {{
+        "G": {{
             "item": "minecraft:glowstone"
-        },
-        "#": {
-            "item": "minecraft:color_dye"
-        },
-        "S": {
+        }},
+        "#": {{
+            "item": "minecraft:{ingredient}"
+        }},
+        "S": {{
             "item": "minecraft:string"
-        }
-    },
-    "result": {
-        "item": "snowyspirit:glow_lights_color",
+        }}
+    }},
+    "result": {{
+        "item": "snowyspirit:glow_lights_{variety}",
         "count": 16
-    }
-}"""
+    }}
+}}"""
 
 colors = [
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "purple",
-    "magenta",
-    "light_blue",
-    "cyan",
-    "lime",
-    "brown",
-    "black",
-    "white",
-    "pink",
-    "gray",
-    "light_gray"
+    ("red_dye", "red"),
+    ("orange_dye", "orange"),
+    ("yellow_dye", "yellow"),
+    ("green_dye", "green"),
+    ("blue_dye", "blue"),
+    ("purple_dye", "purple"),
+    ("magenta_dye", "magenta"),
+    ("light_blue_dye", "light_blue"),
+    ("cyan_dye", "cyan"),
+    ("lime_dye", "lime"),
+    ("brown_dye", "brown"),
+    ("black_dye", "black"),
+    ("white_dye", "white"),
+    ("pink_dye", "pink"),
+    ("gray_dye", "gray"),
+    ("light_gray_dye", "light_gray"),
+    ("diamond", "prismatic")
 ]
 
 for i in colors:
-    with open(f"glow_lights_{i}.json", "w") as file:
-        file.write(template.replace("color", i))
+    with open(f"glow_lights_{i[1]}.json", "w") as file:
+        file.write(template.format(ingredient=i[0], variety=i[1]))
